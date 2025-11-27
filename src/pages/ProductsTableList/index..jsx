531cba721410d37e04e4c2c57f91d04f8130af92
Yousefharara <hrarty37@gmail.com>
2025-11-27 21:25:03 +0200

    Establish pages

---
+++ src/pages/ProductsTableList/index..jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProductsList from '../../components/ProductsList';
+
+const ProductsTableList = () => {
+    return (
+        <div>
+        <p>Products Table !!</p>    
+        <ProductsList />
+        </div>
+    );
+}
+
+export default ProductsTableList;
