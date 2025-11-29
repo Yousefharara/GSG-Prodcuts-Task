595dfae406b7a1f184ef0d456419ba9075709c57
Yousefharara <hrarty37@gmail.com>
2025-11-30 00:34:45 +0200

    refactore: After Enhancing verification code

--- src/components/CodeInput/index.jsx
+++ src/components/CodeInput/index.jsx
@@ -1,4 +1,4 @@
-import { createRef, useEffect, useRef, useState } from "react";
+import { createRef, useEffect, useState } from "react";
 import "./style.css";
 
 const INPUT_DATA = [
@@ -25,16 +25,6 @@ const INPUT_DATA = [
 ];
 
 const CodeInput = () => {
-  const inputRef1 = useRef(null);
-  const inputRef2 = useRef(null);
-  const inputRef3 = useRef(null);
-  const inputRef4 = useRef(null);
-
-  const [input1, setInput1] = useState("");
-  const [input2, setInput2] = useState("");
-  const [input3, setInput3] = useState("");
-  const [input4, setInput4] = useState("");
-
   const [eleRefs, setEleRefs] = useState([]);
   const [inputs, setInputs] = useState([]);
 
@@ -54,51 +44,12 @@ const CodeInput = () => {
     eleRefs[0]?.current.focus();
   }, [eleRefs]);
 
-  const handleOnChange1 = (e) => {
-    let value = e.target.value.toString().slice(-1);
-    setInput1(value);
-    if (value === "") {
-      inputRef1.current.focus();
-    } else inputRef2.current.focus();
-  };
-
-  const handleOnChange2 = (e) => {
-    let value = e.target.value.toString().slice(-1);
-    setInput2(value);
-    if (value === "") {
-      inputRef1.current.focus();
-    } else inputRef3.current.focus();
-  };
-
-  const handleOnChange3 = (e) => {
-    let value = e.target.value.toString().slice(-1);
-    setInput3(value);
-    if (value === "") {
-      inputRef2.current.focus();
-    } else inputRef4.current.focus();
-  };
-
-  const handleOnChange4 = (e) => {
-    let value = e.target.value.toString().slice(-1);
-    console.log(value);
-    setInput4(value);
-    if (value === "") {
-      inputRef3.current.focus();
-    } else inputRef4.current.focus();
-  };
-
   const handleOnChange = (e) => {
-    // console.log(eleRefs[Number(e.target.name - 1)].current.name);
     let value = e.target.value.toString().slice(-1);
     let id = e.target.name; // name = 1
     setInputs((prev) =>
       prev.map((item) => (item.id === +id ? { ...item, value: value } : item))
     );
-    console.log("Value is : ", value);
-    console.log("Ref Value : ", eleRefs[Number(id - 1)].current);
-    console.log(inputs);
-
-    console.log("!value ? : ", !value);
 
     if (!value) {
       console.log("if is true : ", eleRefs[Number(id - 1)]?.current);
@@ -107,20 +58,6 @@ const CodeInput = () => {
       console.log("if is false : ", eleRefs[Number(+id)]?.current);
       eleRefs[Number(+id)]?.current.focus();
     }
-
-    // console.log(inputRef.current);
-    // let value = e.target.value.toString().slice(-1);
-    // console.log(value);
-    // const obj = {
-    //   1: inputRef1,
-    //   2: inputRef2,
-    //   3: inputRef3,
-    //   4: inputRef4,
-    // };
-    // if (value === "") {
-    // } else {
-    //   obj[Number(e.target.name) + 1].current.focus();
-    // }
   };
 
   return (
@@ -136,55 +73,6 @@ const CodeInput = () => {
             name={input.name}
           />
         ))}
-
-        {/* <input
-          //   ref={inputRef1}
-          //   ref={inputRef}
-          ref={eleRefs[0]}
-          //   value={input1}
-          value={inputs[0]?.value || ""}
-          type="text"
-          //   onChange={handleOnChange1}
-          onChange={handleOnChange}
-          //   maxLength={1}
-          name={1}
-        />
-        <input
-          //   ref={inputRef2}
-          //   ref={inputRef}
-          ref={eleRefs[1]}
-          //   value={input2}
-          value={inputs[1]?.value || ""}
-          type="text"
-          //   onChange={handleOnChange2}
-          onChange={handleOnChange}
-          //   maxLength={1}
-          name={2}
-        />
-        <input
-          //   ref={inputRef3}
-          //   ref={inputRef}
-          ref={eleRefs[2]}
-          //   value={input3}
-          value={inputs[2]?.value || ""}
-          type="text"
-          //   onChange={handleOnChange3}
-          onChange={handleOnChange}
-          //   maxLength={1}
-          name={3}
-        />
-        <input
-          //   ref={inputRef4}
-          //   ref={inputRef}
-          ref={eleRefs[3]}
-          //   value={input4}
-          value={inputs[3]?.value || ""}
-          type="text"
-          //   onChange={handleOnChange4}
-          onChange={handleOnChange}
-          name={4}
-          //   maxLength={1}
-        /> */}
       </div>
     </div>
   );
